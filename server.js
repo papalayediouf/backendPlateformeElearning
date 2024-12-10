bdcbf0e17d6fdb6c7ded7dfaef3fd2df62928ba6
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2024-12-10 17:03:51 +0000

    1 commit

--- server.js
+++ server.js
@@ -6,16 +6,10 @@ const app = express();
 // Middleware pour analyser le corps des requêtes JSON
 app.use(express.json());
 
-// Route pour la racine
-app.get('/', (req, res) => {
-    res.send('Bienvenue sur l’API de la plateforme e-learning !');
-});
-
-// Importer les routes
 const allTraining = require('./routes/allTrainings/allTraining');
 app.use('/allTraining', allTraining);
 
-// Lancement du serveur
 app.listen(port, () => {
     console.log("Le serveur est en ligne sur le port", port);
 });
+
